f5541fbc5e66ba365232023491b0d4ca1442a027
meri <merituulie@hotmail.fi>
2023-12-19 18:15:51 +0200

    Use macro for http version

--- src/HttpResponseParser.cpp
+++ src/HttpResponseParser.cpp
@@ -5,14 +5,14 @@ std::string HttpResponseParser::Parse(const HttpResponse &response, Server *serv
 {
 	std::string responseString;
 
-	responseString.append(response.getVersion());
+	responseString.append(HTTP_VERSION);
 	responseString.append(SPACE);
-
 	std::pair<unsigned int, std::string> httpStatus = response.getStatus();
 	responseString.append(ScalarConverter::intToString((int)httpStatus.first));
 	responseString.append(SPACE);
 	responseString.append(httpStatus.second);
 	responseString.append(HTTP_LINEBREAK);
+
 	responseString.append("Date: ");
 	responseString.append(Timer::GetTimeDateString());
 	responseString.append(HTTP_LINEBREAK);
@@ -26,6 +26,7 @@ std::string HttpResponseParser::Parse(const HttpResponse &response, Server *serv
 	responseString.append(response.getContentType());
 	responseString.append(HTTP_LINEBREAK);
 	responseString.append(HTTP_LINEBREAK);
+
 	responseString.append(response.getBody());
 	responseString.append(HTTP_LINEBREAK);
 	responseString.append(HTTP_LINEBREAK);
