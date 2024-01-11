0be0d392a569d1df655e12530493f100a999f0a7
Dan Palmer <dj.palmer81@gmail.com>
2024-01-11 13:26:49 +0200

    needs testing

--- include/ConfigParser.hpp
+++ include/ConfigParser.hpp
@@ -4,31 +4,9 @@
 
 class ConfigParser
 {
-	// private:
-	// 	std::ifstream				configFile;
-	// 	size_t						lineCount;
-	// 	size_t						start;
-	// 	nestedMap					nestedStrings;
-	// 	std::string					stringFile;
-	// 	std::string					execPath;
-	// 	std::vector<iterPair>		pairArray;
-	// 	std::vector<nestedMap>		configFileInfo;
-	// 	std::vector<std::string>	parsed;
-	// 	std::vector<size_t>			ports;
-	// 	std::vector<numberMap>		errors;
+	private:
+		 void processLine(const std::string& line);
 
-	// 	bool						validateDirectory(char **environ, char **av);
-	// 	bool						checkPath(char **environ, char **av);
-	// 	bool						readFile();
-
-	// public:
-	// 	ConfigParser();
-	// 	~ConfigParser();
-
-	// 	// ERRORS
-	// 	class	ErrorEnviron : public std::exception
-	// 	{
-	// 		virtual const char	*what() const throw();
-	// 	};
-
+	public:
+		static void parseConfig(const std::string& filename);
 };
--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -1,14 +1,65 @@
 #include "../include/ConfigParser.hpp"
 
-//ConfigParser::ConfigParser() : lineCount(0), start(0)
-//{
-//}
+void processLine(const std::string& line)
+{
+	std::istringstream iss(line);
+	std::string keyword;
+	iss >> keyword;
 
-//ConfigParser::~ConfigParser()
-//{
-//}
+	if (keyword == "server") 
+	{
+		// Process server information
+		std::cout << "Found server block" << std::endl;
+	} 
+	else if (keyword == "main") 
+	{
+		// Process main information
+		std::cout << "Found main block" << std::endl;
+	} 
+	else if (keyword == "location") 
+	{
+		std::string locationName;
+		iss >> locationName;
+		// Process location information
+		std::cout << "Found location block: " << locationName << std::endl;
+	} 
+	else 
+	{
+		std::cout << "Unknown keyword: " << keyword << std::endl;
+	}
+}
 
-//const char	*ConfigParser::ErrorEnviron::what() const throw()
-//{
-//	return ("Webserv: Cannot find path");
-//}
+static std::string trim(const std::string& str)
+{
+		size_t first = str.find_first_not_of(' ');
+		if (first == std::string::npos) 
+			return "";
+		size_t last = str.find_last_not_of(' ');
+		return str.substr(first, (last - first + 1));
+}
+
+void parseConfig(const std::string& filename) 
+{
+	std::ifstream file(filename);
+	std::string line;
+
+	if (!file.is_open()) {
+		std::cerr << "Failed to open file: " << filename << std::endl;
+		return;
+	}
+
+	while (std::getline(file, line)) 
+	{
+		// Ignore comments
+		size_t commentPos = line.find('#');
+		if (commentPos != std::string::npos)
+			line = line.substr(0, commentPos);
+
+		// Trim whitespace
+		line = trim(line);
+
+		if (!line.empty()) {
+			processLine(line);
+		}
+	}
+}
