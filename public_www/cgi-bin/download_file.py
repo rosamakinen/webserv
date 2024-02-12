b281ec99b0210e557135ea40abe2b61020c75d54
meri <merituulie@hotmail.fi>
2024-02-12 11:34:25 +0200

    Add custom error page and curl tests

---
+++ public_www/cgi-bin/download_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pythons
+
+# HTTP Header
+print("Content-Type:application/octet-stream; name = \"FileName\"\r\n")
+print("Content-Disposition: attachment; filename = \"FileName\"\r\n\n")
+
+# Original File
+my_file = open("textFile.txt", "rb")
+# read the file content
+text = my_file.read()
+print(text)
+
+# Close opened file
+my_file.close()
